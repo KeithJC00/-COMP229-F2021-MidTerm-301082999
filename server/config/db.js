5ae50d3165313af854005787aa637172918e4579
TuHanren <tu.hanren@gmail.com>
2021-10-29 23:15:42 -0400

    midterm first

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Keith:Jc1357911@min-website-005.iunfa.mongodb.net/books229?retryWrites=true&w=majority"
+};
